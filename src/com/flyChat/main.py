5839587a9654e7c965f62e2aae7cd24e410a9be9
wangke <wangke@ADMINIB-FHS3F0I.cn.ibm.com>
2017-06-20 10:59:36 +0800

    update

--- src/com/flyChat/main.py
+++ src/com/flyChat/main.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 '''
 Created on Jun 19, 2017
 
@@ -8,6 +9,7 @@ from flask import Flask, request, Response, jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
+db = None
 
 tasks = [
     {
@@ -24,17 +26,37 @@ tasks = [
     }
 ]
 
-@app.route("/login", methods=['GET'])
-def login():
+
+def getDbConn():
     client = MongoClient('123.57.164.21', 27017)
     db_auth = client.admin
-    db_auth.authenticate("xxxx", "xxxxxx")
-    db=client.mongodb
-    collection=db.user
-    u2 = collection.find_one({'name':'wangke'})
+    db_auth.authenticate("xxxx", "xxxx")
+    global db 
+    db = client.mongodb
+    print '数据库连接正常！'
+
+
+@app.route("/login/<name>/<password>", methods=['GET'])
+def login(name, password):
+    global db
+    collection = db.user
+    u2 = collection.find_one({'name':'wangke', 'password':'123'})
+    print name
+    print password
+    # print request.args.get('abc')
+    return jsonify({'tasks': tasks})
+
+@app.route("/registerUser/<name>/<password>", methods=['GET'])
+def registerUser(name, password):
+    global db
+    collection = db.user
+    u2 = collection.find_one({'name':'wangke', 'password':'123'})
+    print name
+    print password
     print(u2)
-    print request.args.get('abc')
+    # print request.args.get('abc')
     return jsonify({'tasks': tasks})
 
 if __name__ == "__main__":
+    getDbConn()
     app.run(host='127.0.0.1', port=5003)
